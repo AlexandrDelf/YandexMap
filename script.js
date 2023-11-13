fbc92fbaf6dde2f4b0d7944a7b7981f1d3ac2f4b
ADelf <alexandrdelf@gmail.com>
2023-11-13 20:46:19 +0300

    newNew version

--- script.js
+++ script.js
@@ -1,30 +1,49 @@
-initMap();
-
 async function initMap() {
-    // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
     await ymaps3.ready;
 
-    const {YMap, YMapDefaultSchemeLayer} = ymaps3;
+    const {YMap, YMapDefaultSchemeLayer, YMapMarker, YMapFeatureDataSource, YMapLayer } = ymaps3;
 
-    // Иницилиазируем карту
     const map = new YMap(
-        // Передаём ссылку на HTMLElement контейнера
-        document.getElementById('map'),
-
-        // Передаём параметры инициализации карты
+        document.getElementById('app'),
         {
             location: {
-                // Координаты центра карты
-                center: [30.425490, 59.897375],
-
-                // Vасштабирование
-                zoom: 16
+                center: [37.588144, 55.733842],
+                zoom: 10
             }
         }
     );
 
-    // Добавляем слой для отображения схематической карты
     map.addChild(new YMapDefaultSchemeLayer());
 
+    map.addChild(
+        new YMapFeatureDataSource({
+          id: 'markerSource'
+        })
+      );
 
+    map.addChild(
+        new YMapLayer({
+          source: 'markerSource',
+          type: 'markers',
+          zIndex: 2020
+        })
+      );
+
+    const markerElement = document.createElement('div');
+    markerElement.className = 'marker-class';
+    markerElement.innerText = "I'm marker!";
+
+    const marker = new YMapMarker(
+    {
+        source: 'markerSource',
+        coordinates: [37.588144, 55.733842],
+        draggable: true,
+        mapFollowsOnDrag: true
+    },
+    markerElement
+    );
+
+    map.addChild(marker);
 }
+
+initMap();
