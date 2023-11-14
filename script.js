934d5d9900c7f30513afac4d3a096472a38dbad4
ADelf <alexandrdelf@gmail.com>
2023-11-14 17:46:36 +0300

    battleWithMarker

--- script.js
+++ script.js
@@ -1,49 +1,28 @@
 async function initMap() {
-    await ymaps3.ready;
+  await ymaps3.ready;
 
-    const {YMap, YMapDefaultSchemeLayer, YMapMarker, YMapFeatureDataSource, YMapLayer } = ymaps3;
+  const {YMap, YMapDefaultSchemeLayer, YMapMarker, YMapFeatureDataSource, YMapLayer } = ymaps3;
 
-    const map = new YMap(
-        document.getElementById('app'),
-        {
-            location: {
-                center: [37.588144, 55.733842],
-                zoom: 10
-            }
-        }
-    );
+  const map = new YMap(
+      document.getElementById('app'),
+      {
+          location: {
+              center: [37.588144, 55.733842],
+              zoom: 10
+          }
+      }
+  );
 
-    map.addChild(new YMapDefaultSchemeLayer());
+  map.addChild(new YMapDefaultSchemeLayer());
+  map.addChild(new YMapDefaultFeaturesLayer());
 
-    map.addChild(
-        new YMapFeatureDataSource({
-          id: 'markerSource'
-        })
-      );
+//   map.addChild(new YMapFeatureDataSource({id: 'popups'}));
+//   map.addChild(new YMapLayer({source: 'popups'}));
+  
+  
 
-    map.addChild(
-        new YMapLayer({
-          source: 'markerSource',
-          type: 'markers',
-          zIndex: 2020
-        })
-      );
-
-    const markerElement = document.createElement('div');
-    markerElement.className = 'marker-class';
-    markerElement.innerText = "I'm marker!";
-
-    const marker = new YMapMarker(
-    {
-        source: 'markerSource',
-        coordinates: [37.588144, 55.733842],
-        draggable: true,
-        mapFollowsOnDrag: true
-    },
-    markerElement
-    );
-
-    map.addChild(marker);
+  
 }
+
 
 initMap();
